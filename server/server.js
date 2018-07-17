ca5c2214ce9559bc4c5e046457d4b89ab5e1c34a
Matias Calcagno <mcalcagno@velocitypartners1.onmicrosoft.com>
2018-07-17 10:20:34 -0300

    Added basic server.

---
+++ server/server.js
@@ -0,0 +1,49 @@
+require('dotenv').config({ path: '../.env' });
+const app = require('./app');
+const debug = require('debug')('server:server');
+const http = require('http');
+
+function normalizePort(val) {
+  let port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+const server = http.createServer(app);
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+server.listen(port);
+
+server.on('error', error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      throw new Error(bind + ' requires elevated privileges');
+    case 'EADDRINUSE':
+      throw new Error(bind + ' is already in use');
+    default:
+      throw error;
+  }
+});
+
+server.on('listening', () => {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+});
